6d752b3a6e8528421537ff23fbc95cbd213e0214
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-03-04 04:35:18 -0800

    Add project files.

---
+++ TodoAPI/Controllers/SuperHerosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeros.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHerosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            var heros = new List<SuperHero>
+            {
+                 new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York"
+
+
+
+                }
+            };
+
+            return Ok(heros);
+               
+        }
+    }
+}
